c9caef676a3b2f4c12fe0fc8ab95de22638f7a32
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2022-09-12 15:39:53 +0700

    config tailwind, prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,11 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/container/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     // For the best performance and to avoid false positives,
     // be as specific as possible with your content configuration.
   ],
   theme: {
-    screens: {
-      '3xs': {'min': '100px','max':'375px'},
-        '2xs': {'min': '375px','max':'440px'},
-        'xs': {'min': '440px','max':'540px'},
-        's': {'min': '540px','max':'640px'},
-        ...defaultTheme.screens,
-      },
-    extends: {
-      
-    },
+    extends: {},
   },
   plugins: [],
 };
