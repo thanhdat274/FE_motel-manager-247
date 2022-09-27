a3f259329accb9a1a477a0426794f39397a30042
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2022-09-27 09:04:56 +0700

    Create a statistics interface

---
+++ src/components/chart/Bar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+// import faker from 'faker';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Doanh thu hàng tháng',
+    },
+  },
+};
+
+const labels = ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11','Tháng 12'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: '',
+      data: [12, 19, 3, 5, 2, 3,22,33,44,55,66,3],
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    }
+  ],
+};
+const BarChart = () => {
+  return (
+    <div className='block max-w-full'>
+      <Bar options={options} data={data} />
+    </div>
+  );
+};
+export default BarChart;
