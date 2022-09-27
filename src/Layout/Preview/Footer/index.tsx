88ab79719c6afc1776fe0b9c5f9f6a333d03bbe1
dactrong <dactrong2001@gmail.com>
2022-09-27 14:03:41 +0700

    feat ui/introduct

--- src/Layout/Preview/Footer/index.tsx
+++ src/Layout/Preview/Footer/index.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 type Props = {};
 
 const FooterPreview = (props: Props) => {
-  return <div>FooterPreview</div>;
+  return <div className='text-center bg-black'>
+
+    <p className='pt-3 pb-3 text-white'>Thiết kế bởi Maple JSC @2017 - 2022</p>
+  </div>;
 };
 
 export default FooterPreview;
