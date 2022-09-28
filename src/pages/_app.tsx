74f3a0ce2c7a89c2d9694fd4e7b2582894629499
nghiaphamtq17 <nghiaphamtq17@gmail.com>
2022-09-28 11:18:41 +0700

    loading overlay

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,20 +9,13 @@ import ReactLoading from 'react-loading';
 import LayoutTenants from 'src/Layout/Manager/Tenants';
 import LayoutIntro from 'src/Layout/Preview';
 import LayoutListHome from 'src/Layout/ListHome';
+import UserProvider, { useUserContext } from '@/context/UserContext';
 
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const [loading, setLoading] = useState(false);
 
-  // Router.events.on('routeChangeStart', () => {
-  //   setLoading(true);
-  // });
-
-  // Router.events.on('routeChangeComplete', () => {
-  //   setLoading(false);
-  // });
   const switchLayout = () => {
     if (router.pathname.search('/manager/landlord/list-home') >= 0) {
       return (
@@ -35,11 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     if (router.pathname.search('/manager/landlord') >= 0) {
       return (
         <LayoutLandlords>
-          {loading ? (
-            <ReactLoading type={'spinningBubbles'} color="red" width={300} height={300} />
-          ) : (
-            <Component {...pageProps} />
-          )}
+          <Component {...pageProps} />
         </LayoutLandlords>
       );
     }
@@ -61,11 +50,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   };
 
-  // return (
-  // <SessionProvider>
-  //   {switchLayout()}
-  // </SessionProvider>);
-  return <div>{switchLayout()}</div>;
+  return <UserProvider>{switchLayout()}</UserProvider>;
 }
 
 export default MyApp;
