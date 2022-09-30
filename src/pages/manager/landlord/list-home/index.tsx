19534231dbf7fe52378f6fb20c6a496a0736f3d4
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2022-09-30 21:58:59 +0700

    update

--- src/pages/manager/landlord/list-home/index.tsx
+++ src/pages/manager/landlord/list-home/index.tsx
@@ -4,13 +4,14 @@ import Link from 'next/link';
 import { supabase } from 'src/apis/supabase';
 import axios from 'axios';
 import { Toast } from 'src/hooks/toast';
+import { useUserContext } from '@/context/UserContext';
 
 const ListHome = () => {
+  const { setLoading } = useUserContext();
   const [house, setHouse] = useState([]);
-  const [openModal, setOpenModal] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [changeData, setChangeData] = useState(0);
-
+  const router = useRouter();
+  const { id } = router.query;
+  console.log('id', id);
   // const getHouse = async () => {
   //   try {
   //     const res = await supabase.from('houses').select('*');
@@ -24,49 +25,36 @@ const ListHome = () => {
   //   } catch (error) {}
   // };
 
-  const getHouse = async () => {
-    try {
-      const res = await axios.get('https://633505ceea0de5318a0bacba.mockapi.io/api/house');
-      if (res.data) {
-        setHouse(res.data as any);
-        console.log('data', res.data);
-      }
-    } catch (error) {
-      console.log('error', error);
-    }
-  };
-
   useEffect(() => {
     console.log('run');
-
+    const getHouse = async () => {
+      try {
+        const res = await axios.get('https://633505ceea0de5318a0bacba.mockapi.io/api/house');
+        if (res.data) {
+          setHouse(res.data as any);
+          console.log('data', res.data);
+        }
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
     getHouse();
-  }, [changeData]);
-  const router = useRouter();
-
-  const { id } = router.query;
-  console.log('id', id);
+  }, []);
 
   const removeHouse = async (id: number) => {
+    console.log(id);
+    setLoading(true);
     try {
-      await supabase
-        .from('houses')
-        .delete()
-        .match({ id })
-        .then(() => {
-          Toast('success', 'Xóa phòng thành công');
-          setChangeData(changeData + 1);
-          router.push('/manager/landlord/list-home');
-        });
+      await axios.delete('https://633505ceea0de5318a0bacba.mockapi.io/api/house/' + id).then(() => {
+        Toast('success', 'Xóa nhà thành công');
+        setHouse(house.filter((item: any) => item.id !== id));
+        setLoading(false);
+      });
     } catch (error) {
-      Toast('success', 'Xóa phòng không thành công');
+      Toast('success', 'Xóa nhà không thành công');
+      setLoading(false);
     }
   };
-
-  // console.log(house);
-
-  // console.log('landlord', router.pathname.search('/manager/landlord'));
-
-  // console.log('ternant', router.pathname.search('/manager/ternant'));
 
   const renderList = () => {
     return (
@@ -87,23 +75,23 @@ const ListHome = () => {
           </div>
           <div className=" my-4 mx-4  sm:grid sm:grid-cols-2  sm:gap-2 lg:grid lg:grid-cols-4  lg:gap-4 ">
             {house &&
-              house.map((item: { id: number; name: string; address: string }, index: React.Key | null | undefined) => {
+              house.map((item: any, index: React.Key | null | undefined) => {
                 return (
                   <>
                     <div className="border-2 text-center  py-8 bg-gray-100  mt-3" key={index}>
                       <div className="">
                         <h1 className="">
                           <span className="bg-sky-400 border text-lg hover:bg-cyan-500 text-white hover:text-black rounded-md  font-bold p-3">
-                            {item?.name}
+                            {item.name}
                           </span>
                         </h1>
                       </div>
                       <div className="m-3 border bg-slate-300 rounded-md text-left ">
-                        <p className="p-2">{item?.address}</p>
+                        <p className="p-2">{item.address}</p>
                       </div>
                       <div>
                         <div>
-                          <Link href={`${item?.id}`}>
+                          <Link href={`${item.id}`}>
                             <a className="border  pr-2 pl-2 pt-1 pb-1 rounded-md bg-emerald-500 text-white hover:bg-green-800">
                               Quản lý
                             </a>
@@ -114,7 +102,7 @@ const ListHome = () => {
                             </a>
                           </Link>
                           <button
-                            onClick={() => removeHouse(item?.id)}
+                            onClick={() => removeHouse(item.id)}
                             className="border pr-2 pl-2 pt-1 pb-1 rounded-md bg-rose-600 text-white hover:bg-rose-800 "
                           >
                             Xóa
