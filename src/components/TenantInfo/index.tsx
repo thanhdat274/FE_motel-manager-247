1920f0f0f7f049e89bc7e61ea64513a230c30c44
nghiaphamtq17 <nghiaphamtq17@gmail.com>
2022-10-03 18:11:55 +0700

    create room information

---
+++ src/components/TenantInfo/index.tsx
@@ -0,0 +1,171 @@
+import { useUserContext } from '@/context/UserContext';
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Toast } from 'src/hooks/toast';
+
+type IForm = {
+  name: string;
+  price: number;
+  status: boolean;
+  max: number;
+};
+type Props = {
+  data: IForm | any;
+};
+
+const TenantInformation = ({ data }: any) => {
+  const { name, price, status, max, area } = data;
+  const router = useRouter();
+  const param = router.query;
+
+  const { setLoading } = useUserContext();
+
+  useEffect(() => {
+    if (data) {
+      reset(data);
+    }
+  }, [data]);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: name,
+      price: price,
+      status: status,
+      max: max,
+      area: area,
+    },
+  });
+  const onSubmit = async (data: any) => {
+    setLoading(true);
+    try {
+      await axios
+        .put(`https://633505ceea0de5318a0bacba.mockapi.io/api/house/${param.id}/room/` + `${param.id_room}`, data)
+        .then(() => {
+          setLoading(false);
+          router.push(`/manager/landlord/${param.id}/list-room`);
+          Toast('success', 'Cập nhật phòng thành công');
+        });
+    } catch (error) {
+      Toast('error', 'Cập nhật phòng không thành công');
+    }
+  };
+
+  return (
+    <div className="max-w-full mx-auto py-6 ">
+      <div className="md:grid md:grid-cols-3 md:gap-6">
+        <div className="mt-5 md:mt-0 md:col-span-3 border rounded-md">
+          <form id="formAdd" onSubmit={handleSubmit(onSubmit)}>
+            <div className="shadow rounded-md overflow-hidden">
+              <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                <div className="col-span-6">
+                  <label className="block text-sm font-bold text-gray-700" htmlFor="username">
+                    Tên phòng <span className="text-[red]">*</span>
+                  </label>
+                  <input
+                    className="mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    {...register('name', { required: true, minLength: 6 })}
+                  />
+                  {errors.name?.type === 'required' && (
+                    <span className="text-[red] mt-1 block">Không dược để trống!</span>
+                  )}
+                  {errors.name?.type === 'minLength' && (
+                    <span className="text-[red] mt-1 block">Tối thiểu 6 ký tự</span>
+                  )}
+                </div>
+
+                <div className="col-span-6">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="username">
+                    Trạng thái phòng
+                  </label>
+                  <select
+                    className="mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    {...register('status', { required: true })}
+                    id="status"
+                  >
+                    <option value="true">Sẵn sàng</option>
+                    <option value="false">Chưa sẵn sàng</option>
+                  </select>
+                </div>
+
+                <div className="col-span-6">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="username">
+                    Giá phòng <span className="text-[red]">*</span>
+                  </label>
+                  <input
+                    className="mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="price"
+                    type="number"
+                    {...register('price', { required: true, min: 1000 })}
+                  />
+                  {errors.price && errors.price.type === 'required' && (
+                    <span className="text-[red] mt-1 block">Không dược để trống!</span>
+                  )}
+                </div>
+
+                <div className="col-span-6">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="username">
+                    Số người ở tối đa <span className="text-[red]">*</span>
+                  </label>
+                  <input
+                    className="mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="max"
+                    type="number"
+                    {...register('max', { required: true })}
+                  />
+                  {errors.max && errors.max.type === 'required' && (
+                    <span className="text-[red] mt-1 block">Không dược để trống!</span>
+                  )}
+                </div>
+
+                <div className="col-span-6">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="username">
+                    Diện tích <span className="text-[red]">*</span>
+                  </label>
+                  <input
+                    className="mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="area"
+                    type="number"
+                    {...register('area', { required: true })}
+                  />
+                  {errors.area && errors.area.type === 'required' && (
+                    <span className="text-[red] mt-1 block">Không dược để trống!</span>
+                  )}
+                </div>
+              </div>
+
+              <div className="px-4 py-3 flex gap-[20px] bg-gray-50 text-right sm:px-6 ">
+                <Link
+                  href={`/manager/landlord/${param.id}/list-room`}
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <a className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Quay lại
+                  </a>
+                </Link>
+
+                <button
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Lưu
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TenantInformation;
