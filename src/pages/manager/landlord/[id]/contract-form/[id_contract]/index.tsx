58c766ee7112e36cff3b0fa0f2280f7b8b682392
dactrong <dactrong2001@gmail.com>
2022-10-06 21:18:46 +0700

    update contracForm

--- src/pages/manager/landlord/[id]/contract-form/[id_contract]/index.tsx
+++ src/pages/manager/landlord/[id]/contract-form/[id_contract]/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import 'react-quill/dist/quill.snow.css';
 import dynamic from 'next/dynamic';
+import 'react-quill/dist/quill.snow.css';
 import { useRouter } from 'next/router';
 import { useUserContext } from '@/context/UserContext';
 import { useForm } from 'react-hook-form';
@@ -8,6 +8,7 @@ import axios from 'axios';
 import { Toast } from 'src/hooks/toast';
 
 type Props = {};
+
 const ReactQuill = dynamic(import('react-quill'), { ssr: false });
 const ContracEdit = (props: Props) => {
   const modules = {
@@ -22,6 +23,8 @@ const ContracEdit = (props: Props) => {
   };
   const router = useRouter();
   const param = router.query;
+  const { setLoading } = useUserContext();
+
   console.log(param);
 
   const {
@@ -40,6 +43,7 @@ const ContracEdit = (props: Props) => {
     register('contract', { required: true, minLength: 15 });
   }, [register]);
   useEffect(() => {
+    setLoading(true)
     const getRoom = async () => {
       try {
         const res = await axios.get(
@@ -47,6 +51,7 @@ const ContracEdit = (props: Props) => {
         );
         if (res.data) {
           reset(res.data as any);
+          setLoading(false)
           console.log('data', res.data);
         }
       } catch (error) {
@@ -57,25 +62,27 @@ const ContracEdit = (props: Props) => {
   }, [param.id_room]);
   const contract1 = watch('contract');
   const onSubmit = async (data: any) => {
+    setLoading(true)
     console.log(data);
     try {
       await axios
         .put(`https://6332ba04a54a0e83d2570a0f.mockapi.io/api/contract-form/` + `${param.id_contract}`, data)
         .then((data: any) => {
           router.push(`/manager/landlord/${param.id}/contract-form`);
-          Toast('success', 'Cập nhật phòng thành công');
+          Toast('success', 'Cập nhật hợp đồng thành công');
         });
     } catch (error) {
-      Toast('error', 'Cập nhật phòng không thành công');
+      Toast('error', 'Cập nhật hợp đồng không thành công');
     }
   };
   return (
     <div>
       <form action="" onSubmit={handleSubmit(onSubmit)}>
+      <button type="submit" className='bg-cyan-400 text-white rounded-md px-5 py-3  mb-5 hover:bg-cyan-500'>Cập nhật</button>
         <ReactQuill theme="snow" modules={modules} value={contract1} onChange={onEditorStateChange} />
         <h1 className="Error">{errors.contract && 'Bạn phải nhập trường này'}</h1>
 
-        <button type="submit">Thêm</button>
+        
       </form>
     </div>
   );
--- src/pages/manager/landlord/[id]/contract-form/index.tsx
+++ src/pages/manager/landlord/[id]/contract-form/index.tsx
@@ -2,22 +2,28 @@ import axios from 'axios';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
-
+import dynamic from 'next/dynamic';
+import { useUserContext } from '@/context/UserContext';
 type Props = {};
 
 const ContractForm = (props: Props) => {
+  
+const ReactQuill = dynamic(import('react-quill'), { ssr: false });
   const [contract1, setContract] = useState([]);
+  const { setLoading } = useUserContext();
+
   // console.log(contract1);
   const router = useRouter();
   const param = router.query;
   console.log(param);
 
   useEffect(() => {
+    setLoading(true)
     const getContract = async () => {
       const { data } = await axios.get('https://6332ba04a54a0e83d2570a0f.mockapi.io/api/contract-form');
       setContract(data as any);
+      setLoading(false)
     };
     getContract();
   }, []);
@@ -31,11 +37,11 @@ const ContractForm = (props: Props) => {
             <h1 key={index}> </h1>
             <p className="">
               <Link href={`/manager/landlord/${param.id}/contract-form/${item.id}/`}>
-                <a className="border ">Sửa</a>
+                <a  className='bg-cyan-400 text-white rounded-md px-5 py-3  mb-5 hover:bg-cyan-500'>Cập nhật</a>
               </Link>
             </p>
 
-            <ReactQuill value={item?.contract} readOnly={true} theme={'bubble'} />
+            <ReactQuill value={item?.contract} readOnly={true} theme={'bubble'} className="border mt-5"/>
           </>
         );
       })}
