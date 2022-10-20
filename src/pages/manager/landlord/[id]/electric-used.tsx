a94c61b50d5e2203587f1e8aa27eab7edca9e588
dacphuongotp <dacphuongotp@gmail.com>
2022-10-20 09:43:43 +0700

    update

--- src/pages/manager/landlord/[id]/electric-used.tsx
+++ src/pages/manager/landlord/[id]/electric-used.tsx
@@ -5,11 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faFileExcel, faSave, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useUserContext } from '@/context/UserContext';
 import { listRoom } from 'src/pages/api/room';
+// import { MonthPicker, Row, Col } from 'uiw';
 // import 'react-month-picker/css/'
 
 type Props = {};
 
 const LisElectric = (props: Props) => {
+
   // const [formatDate, setFormatDate] = useState('2019/04');
   // const onChange = (date, formatDate) => {
   //   setFormatDate(formatDate);
@@ -37,6 +39,7 @@ const LisElectric = (props: Props) => {
     };
     getRoom();
   }, [id]);
+  console.log(rooms);
 
   const submitHandleElectric = async (id: any) => {};
 
@@ -95,90 +98,111 @@ const LisElectric = (props: Props) => {
               Thống kê số điện
             </h6>
           </div>
-          <div className="flex-auto px-0 pt-0 pb-2  overflow-x-scroll overflow-y-scroll">
-            <div className="p-0">
-              <table className="items-center w-full m-2 align-top border-gray-200 text-slate-500">
-                <thead className="align-bottom">
-                  <tr>
-                    <th className="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border border-gray-200 shadow-none text-xxs border-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                      Nhà
-                    </th>
-                    <th className="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border border-gray-200 shadow-none text-xxs border-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                      Phòng
-                    </th>
-                    <th className="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border border-gray-200 shadow-none text-xxs border-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                      Khách thuê
-                    </th>
-                    <th className="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border border-gray-200 shadow-none text-xxs border-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                      Chỉ số điện Cũ
-                    </th>
-                    <th className="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border border-gray-200 shadow-none text-xxs border-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                      Chỉ số điện Mới
-                    </th>
-                    <th className="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border border-gray-200 shadow-none text-xxs border-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                      Sử dụng
-                    </th>
-                    <th className="px-6 py-3 font-semibold capitalize align-middle bg-transparent border border-gray-200 border-solid shadow-none tracking-none whitespace-nowrap text-slate-400 opacity-70" />
-                  </tr>
-                </thead>
-                <tbody>
-                  {rooms?.map((room, index) => {
-                    return (
-                      <tr key={index} className="border">
-                        <td className="p-2 align-middle bg-transparent border whitespace-nowrap shadow-transparent">
-                          <div className="flex px-2 py-1">
-                            <div className="flex flex-col justify-center">
-                              <h6 className="mb-0 leading-normal text-sm">Nhà 1</h6>
-                            </div>
-                          </div>
-                        </td>
-
-                        <td className="p-2 align-middle bg-transparent border whitespace-nowrap shadow-transparent">
-                          <p className="mb-0 font-semibold leading-tight text-sm">{room.name}</p>
-                        </td>
-
-                        <td className="p-2 leading-normal text-center align-middle bg-transparent border text-sm whitespace-nowrap shadow-transparent">
-                          <span className=" from-emerald-500 to-teal-400 px-3.6 text-sm rounded-1.8 py-2.2 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-black">
-                            Nguyễn Đắc Phương
-                          </span>
-                        </td>
-
-                        <td className="p-2 text-center align-middle bg-transparent border whitespace-nowrap shadow-transparent">
-                          <span className="font-bold w-full flex border-0 px-3 py-3 placeholder-blueGray-300 text-green-700 bg-gray-200  rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150">
-                            {room.elictric_number}
-                          </span>
-                        </td>
-
-                        <td className="p-2 text-center align-middle bg-transparent border whitespace-nowrap shadow-transparent">
-                          <input
-                            placeholder={room.update_elictric_number}
-                            name="update_elictric_number"
-                            className="font-bold w-full flex border-0 px-3 py-3 placeholder-blueGray-300 text-red-900 bg-gray-200  rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150"
-                            type="number"
-                          />
-                        </td>
-
-                        <td className="p-2 text-center align-middle bg-transparent border whitespace-nowrap shadow-transparent">
-                          <span className="font-semibold leading-tight text-sm text-slate-400">
-                            {room.update_elictric_number - room.elictric_number}
-                          </span>
-                        </td>
-
-                        <td className="p-2 bg-transparent shadow-transparent">
-                          <button
-                            onClick={() => submitHandleElectric(room.id)}
-                            className="border flex rounded px-3 py-1 items-center bg-sky-400"
-                            type="button"
+          <div className="max-w-full mx-2 py-6 sm:px-6 lg:px-8">
+            <div className="flex flex-col">
+              <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full ">
+                  <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table className="w-full divide-y divide-gray-200">
+                      <thead className="bg-gray-50">
+                        <tr>
+                          <th
+                            scope="col"
+                            className="px-9 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
                           >
-                            <FontAwesomeIcon className="w-[16px] text-black" icon={faSave} />
-                            <span className="ml-1">Lưu</span>
-                          </button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+                            Nhà
+                          </th>
+
+                          <th
+                            scope="col"
+                            className="px-9 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Phòng
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-9 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Khách thuê
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-9 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Số điện cũ
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-9 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Số điện mới
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-9 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Sử dụng
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-9 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Lưu
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="bg-white divide-y divide-gray-200">
+                        {rooms?.map((room, index) => {
+                          return (
+                            <tr key={index}>
+                              <td className="px-9 py-4 whitespace text-sm text-gray-500">
+                                <div className="text-center">Nhà</div>
+                              </td>
+                              <td className="px-6 py-4 whitespace">
+                                <div className="text-center">{room.name}</div>
+                              </td>
+                              <td className="px-6 py-4 whitespace">
+                                <div className="text-center">Nguyễn Đắc Phương</div>
+                              </td>
+                              <td className="px-6 py-4 whitespace">
+                                <div className="text-center">
+                                  <span className="font-bold w-full flex border-0 px-3 py-3 placeholder-blueGray-300 text-green-700 bg-gray-200  rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150">
+                                    {room.elictric_number}
+                                  </span>
+                                </div>
+                              </td>
+                              <td className="px-6 py-4 whitespace">
+                                <div className="text-center">
+                                  <input
+                                    placeholder={room.update_elictric_number}
+                                    name="update_elictric_number"
+                                    className="font-bold w-full flex border-0 px-3 py-3 placeholder-blueGray-300 text-red-900 bg-gray-200  rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150"
+                                    type="number"
+                                  />
+                                </div>
+                              </td>
+                              <td className="px-6 py-4 whitespace">
+                                <div className="text-center">{room.update_elictric_number - room.elictric_number}</div>
+                              </td>
+
+                              <td className="px-6 py-4 whitespace-nowrap text-center">
+                                <button
+                                  onClick={() => submitHandleElectric(room.id)}
+                                  className="border flex rounded px-3 py-1 items-center bg-sky-400"
+                                  type="button"
+                                >
+                                  <FontAwesomeIcon className="w-[16px] text-black" icon={faSave} />
+                                  <span className="ml-1">Lưu</span>
+                                </button>
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
