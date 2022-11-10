89a121bdce35f04b4e32b881379114cade44bbdb
dacphuongotp <dacphuongotp@gmail.com>
2022-11-10 10:55:24 +0700

    update

--- src/pages/manager/ternant/electricity/index.tsx
+++ src/pages/manager/ternant/electricity/index.tsx
@@ -1,9 +1,39 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllBillForHouse } from 'src/pages/api/billService';
 
 type Props = {};
 
 const listElectricity = (props: Props) => {
+  var NameBuild = 'dien';
+  if (typeof window !== 'undefined') {
+    const [listBillData, setListBillData] = useState<any>([]);
+    const codeRooms = JSON.parse(localStorage.getItem('code_room') as string);
+    console.log(codeRooms._id);
+    console.log(NameBuild);
+
+    useEffect(() => {
+      const getListBillData = async () => {
+        // const {data} = await getAllBillForHouse(NameBuild, monthCheck, yearCheck, id)
+      };
+      getListBillData();
+    }, []);
+  }
+
+  // const codeRooms = JSON.parse(localStorage.getItem('code_room') as string)
+  // console.log(codeRooms);
+
+  // const [rooms, setRooms] = useState();
+  // var codeRooms = 'admin123';
+  // useEffect(() => {
+  //   const getRoom = async () => {
+  //     const { data } = await getRoomBySubName(codeRooms);
+  //     setRooms(data.data);
+  //   };
+  //   getRoom();
+  // }, []);
+  // console.log(demo);
+
   return (
     <div className="h-screen">
       <header className="bg-white shadow">
@@ -11,7 +41,7 @@ const listElectricity = (props: Props) => {
           <div className="lg:flex lg:items-center lg:justify-between">
             <div className="flex-1 min-w-0">
               <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-2xl sm:truncate uppercase">
-                quản lý số điện hàng tháng
+                Số lượng điện tiêu thụ hàng tháng
               </h2>
             </div>
             <div className="mt-5 flex lg:mt-0 lg:ml-4">
