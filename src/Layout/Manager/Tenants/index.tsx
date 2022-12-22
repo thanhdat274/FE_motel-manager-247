8ec2a8f3aee0cdcedcc654d69ef9fe2dc3d0d4ec
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2022-12-22 09:31:47 +0700

    update code

--- src/Layout/Manager/Tenants/index.tsx
+++ src/Layout/Manager/Tenants/index.tsx
@@ -1,5 +1,4 @@
 import Navbar from '@/components/AdminNavbar';
-import NavbarTennants from '@/components/NavBarTennants';
 import SideBarTenants from '@/components/SideBarTenants';
 import { useUserContext } from '@/context/UserContext';
 import React, { ReactNode } from 'react';
@@ -17,7 +16,7 @@ const LayoutTenants = ({ children }: Props) => {
       {<CircleSpinnerOverlay loading={loading} color="#2563eb" size={100} message="Loadinggg" zIndex={9999} />}
 
       <SideBarTenants />
-      <NavbarTennants />
+      <Navbar />
       <div className="relative md:ml-64 bg-blueGray-100">
         <div className=" mx-auto w-full h-full">
           <div className="bg-gray-100 p-4 min-h-screen sm:mt-[55px] s:mt-[55px] 2xs:mt-[55px] xs:mt-[55px] md:mt-0">
