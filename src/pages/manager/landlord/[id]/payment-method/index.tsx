a653c8f6392be63fc89daa9679d9ba75406fe398
nghiaphamtq17 <nghiaphamtq17@gmail.com>
2023-01-09 11:32:18 +0700

    update api payment

--- src/pages/manager/landlord/[id]/payment-method/index.tsx
+++ src/pages/manager/landlord/[id]/payment-method/index.tsx
@@ -1,16 +1,37 @@
 import TabPanelComponent from '@/components/TabPanel';
-import React from 'react'
-import PaymentHistory from './payment-history'
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { getListBill } from 'src/pages/api/payment';
+import PaymentHistory from './payment-history';
 import SettingPayment from './setting-payment';
 
-type Props = {}
+const Payment = () => {
+    const router = useRouter();
+    const { id } = router.query;
+    const [listData, setListData] = useState([]);
 
-const Payment = (props: Props) => {
+    const getReport = async () => {
+        await getListBill(id)
+            .then(({ data }) => {
+                setListData(data?.data);
+                // console.log('data', data?.data);
+            })
+            .catch((err) => {
+                console.log('err');
+            });
+    };
+
+    useEffect(() => {
+        if (id) {
+            getReport();
+        }
+    }, []);
+
     const data = [
         {
             label: 'Lịch sử thanh toán',
             value: 0,
-            children: <PaymentHistory />
+            children: <PaymentHistory listData={listData} />,
         },
 
         {
@@ -18,14 +39,13 @@ const Payment = (props: Props) => {
             value: 1,
             children: <SettingPayment />,
         },
-
     ];
     return (
-        <div className='container'>
+        <div className="container">
             <h2>Payment History</h2>
             <TabPanelComponent data={data} />
         </div>
-    )
-}
+    );
+};
 
-export default Payment
+export default Payment;
